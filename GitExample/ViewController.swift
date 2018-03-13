c3654059e24136e64862ec6ead0d40573fdf85bf
Deepika Kudaka <deepikakudaka@deepikas-macbook-pro.local>
2018-03-13 12:23:56 +0530

    First commit

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonTap(_ sender: Any) {
+        print("button Tapped")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
