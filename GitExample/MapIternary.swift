a56e50528ce89e06a8ee99a05c4322c7f11aa618
Deepika Kudaka <deepikakudaka@deepikas-macbook-pro.local>
2018-03-13 12:26:04 +0530

    new file added

---
+++ GitExample/MapIternary.swift
@@ -0,0 +1,13 @@
+//
+//  MapIternary.swift
+//  GitExample
+//
+//  Created by Deepika Kudaka on 13/03/18.
+//  Copyright © 2018 Deepika Kudaka. All rights reserved.
+//
+
+import UIKit
+
+class MapIternary: NSObject {
+
+}
